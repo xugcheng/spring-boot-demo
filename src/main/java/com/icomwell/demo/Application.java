b2a3d0029231c452305a3b1cce21f937e96883fe
xuguocheng <xuguocheng@icomwell.com>
2016-11-15 19:05:33 +0800

    私有配置

--- src/main/java/com/icomwell/demo/Application.java
+++ src/main/java/com/icomwell/demo/Application.java
@@ -1,7 +1,12 @@
 package com.icomwell.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @EnableAutoConfiguration
+@EnableConfigurationProperties({PrivilegeSettings.class})
 public class Application {
+
+    @Autowired
+    private PrivilegeSettings settings;
 
     @RequestMapping("/")
     public String home(){
         return "Hello World!";
     }
 
+    @RequestMapping("/hello")
+    public String hello(){
+        return String.format("Hello %s,%s",settings.getName(),settings.getAge());
+    }
+
     public static void main(String[] args){
-        SpringApplication.run(Application.class,args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class,args);
+
     }
 }
---
+++ src/main/java/com/icomwell/demo/PrivilegeSettings.java
@@ -0,0 +1,28 @@
+package com.icomwell.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by xuguocheng on 2016/11/15.
+ */
+@ConfigurationProperties(prefix = "privilege", locations = "classpath:config/privilege.properties")
+public class PrivilegeSettings {
+    private String name;
+    private Integer age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
